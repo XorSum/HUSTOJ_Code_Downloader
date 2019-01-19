68e5f3ade186c083d90f1e9acdb2a9b5e29f7d55
xorsum <1779754921@qq.com>
2019-01-19 18:31:52 +0800

    变量重命名

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import re
 import codecs
 import json
 import queue
-import pymysql
+
 from bs4 import BeautifulSoup
 
 from user import read_user_config
@@ -24,7 +24,7 @@ interva_time = 0.1
 
 threadLock = threading.Lock()
 
-# sid_queue = queue.Queue()
+sid_queue = queue.Queue()
 
 
 def result_convert(num):
@@ -45,115 +45,63 @@ def result_convert(num):
     }
     return con[num]
 
-def create_table():
-    # conn = sqlite3.connect('test.db')
-    conn = pymysql.connect("localhost","root","123456","icpc")
-    c = conn.cursor()
-    # c.execute("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='sids';")
-    # table_num = c.fetchone()[0]
-    # if table_num < 1:
-    c.execute("drop table if exists sids")
-    c.execute(''' create table sids (
-             sid int primary key not null ,
-             uid char(50) not null,
-             pid int not null ,
-             result char(20) not null ,
-             memory int not null ,
-             time int not null ,
-             language char(20) not null ,
-             length int not null ,
-             submit_time datetime  not null 
-          );''')
-    #     print("create table")
-    # else:
-    #     print("already have")
-    c.close()
-    conn.commit()
-    conn.close()
-
-
-
 
 def get_sids(url, user_id, cookies):
     top_sid = "-1"
-    flag1 = True
+    isEnd = False
     isFirstPage = True
-    conn = pymysql.connect("localhost", "root", "123456", "icpc")
-
-    # earlest_time = time.mktime(time.strptime("2011-01-17 20:48:07", "%Y-%m-%d %H:%M:%S"))
-    earlest_time =  "2018-02-16 00:00:00"
-
-    while flag1:
-        
-        cursor = conn.cursor()
-        page = requests.get(url=url + "/status.php?user_id="+ user_id+"&top=" + top_sid,
+    while isEnd == False:
+        page = requests.get(url=url + "/status.php", params={"user_id": user_id, "top": top_sid},
                             cookies=cookies, headers=headers)
         soup = BeautifulSoup(page.content, 'lxml')
-        sids = []
-        for trr in soup.find_all('tr', class_={"evenrow", "oddrow"}):
-            lst = trr.find_all('td')
-            content = {}
-            content["sid"] = lst[0].string
-            content["uid"] = lst[1].find('a').string
-            content["pid"] = lst[2].find('div').find('a').string
-            content["result"] = result_convert(lst[3].find('span')['result'])
-            if lst[4].find('div') == None:
-                content["memory"] = "-1"
-                content["time"] = "-1"
-                content["language"] = "-1"
-                content["length"] = "-1"
+        submits = []
+        for row in soup.find_all('tr', class_={"evenrow", "oddrow"}):
+            cols = row.find_all('td')
+            submit = {}
+            submit["sid"] = cols[0].string
+            submit["uid"] = cols[1].find('a').string
+            submit["pid"] = cols[2].find('div').find('a').string
+            submit["result"] = result_convert(cols[3].find('span')['result'])
+            if cols[4].find('div') == None:
+                submit["memory"] = "-1"
+                submit["time"] = "-1"
+                submit["language"] = "-1"
+                submit["length"] = "-1"
             else:
-                content["memory"] = lst[4].find('div').string
-                content["time"] = lst[5].find('div').string
-                if lst[6].find('a') == None:
-                    content["language"] = lst[6].string
+                submit["memory"] = cols[4].find('div').string
+                submit["time"] = cols[5].find('div').string
+                if cols[6].find('a') == None:
+                    submit["language"] = cols[6].string
                 else:
-                    content["language"] = lst[6].find('a').string
-                content["length"] = lst[7].string[0:-2]
+                    submit["language"] = cols[6].find('a').string
+                submit["length"] = cols[7].string[0:-2]
 
-            content["submit_time"] = lst[8].string
-            # content["datetime"] = time.mktime(time.strptime(lst[8].string, "%Y-%m-%d %H:%M:%S"))
-            sids.append(content)
-            print(content)
-            print()
-
-        if len(sids) > 0:
+            submit["submit_time"] = cols[8].string
+            # submit["datetime"] = time.mktime(time.strptime(cols[8].string, "%Y-%m-%d %H:%M:%S"))
+            submits.append(submit)
+            print(submit)
+        if len(submits) > 0: # 如果不是首页，则需要去除重复的第一项
             if isFirstPage == True:
                 isFirstPage = False
             else:
-                sids.remove(sids[0])
-        if len(sids) > 0:
-            for content in sids:
-                # sid_queue.put(content)
-                # current_time = time.mktime(time.strptime(content["datetime"], "%Y-%m-%d %H:%M:%S"))
+                submits.remove(submits[0])
 
-                if content["submit_time"] < earlest_time :
-                    flag1 = False
-                    break
-                try:
-                    cursor.execute("insert into sids(sid,uid,pid,result,memory,time,language,length,submit_time) values (%s,%s,%s,%s,%s,%s,%s,%s,%s)",
-                                (content["sid"],content["uid"],content["pid"],content["result"],content["memory"],content["time"],content["language"],content["length"],content["submit_time"]) )
-                except :
-                    print("database insert error")
-            top_sid = sids[-1]["sid"]
-        else:
-            flag1 = False
-        cursor.close()
-        conn.commit()
-    conn.close()
+        if len(submits) > 0:
+            sid_queue.put(submits)
+            top_sid = submits[-1]["sid"]
+        else:   # 没爬到数据，则结束
+            isEnd = True
+
 
 def main():
-
-    # create_table()
-
     users = read_user_config("./config.json")
     for user in users:
         cookies = login(user.url, user.user_id, user.user_password)
         print("cookies=", cookies)
         get_sids(user.url, user.user_id, cookies)
+
+
         logout(user.url, cookies)
-
-
 
 
 if __name__ == '__main__':
